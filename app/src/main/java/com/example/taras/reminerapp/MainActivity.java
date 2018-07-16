924f0c5b3f03bea6baeedd5b23a190a13c313e28
Taras Koloshmatin <taras@pier2marketing.com>
2018-07-16 20:44:57 +0300

    added toolbar

--- app/src/main/java/com/example/taras/reminerapp/MainActivity.java
+++ app/src/main/java/com/example/taras/reminerapp/MainActivity.java
@@ -1,15 +1,33 @@
 package com.example.taras.reminerapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        mToolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(mToolbar);
+        mToolbar.setTitle(R.string.app_name);
+        mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return false;
+            }
+        });
+
+        mToolbar.inflateMenu(R.menu.menu);
     }
 }
